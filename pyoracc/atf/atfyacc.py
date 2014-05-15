0d43262d56f7def272cff742d2a0ec5914e5d392
James Hetherington <j.hetherington@ucl.ac.uk>
2014-05-15 15:27:16 +0100

    Allow parser to start with subset of rules, for testing

--- pyoracc/atf/atfyacc.py
+++ pyoracc/atf/atfyacc.py
@@ -4,16 +4,16 @@ from ..artifact import Artifact
 
 class AtfParser(object):
   tokens=AtfLexer.tokens
-  def __init__(self):
-    self.parser=yacc.yacc(module=self)
+  def __init__(self,start=None):
+    self.parser=yacc.yacc(module=self,start=start)
 
 
   def p_document(self,p):
-    "document : codeline"
+    "document : code"
     p[0]=p[1]
 
   def p_codeline(self,p):
-    "codeline : CODE ID NEWLINE"
+    "code : CODE ID NEWLINE"
     p[0]=Artifact()
     p[0].code=p[1]
     p[0].description=p[2]
--- pyoracc/test/atf/test_atfparser.py
+++ pyoracc/test/atf/test_atfparser.py
@@ -10,13 +10,13 @@ from fixtures import belsunu
 class testParser(object):
   def setUp(self):
     self.lexer=AtfLexer().lexer
-    self.parser=AtfParser().parser
 
-  def try_parse(self,content):
+  def try_parse(self,content,start):
+      self.parser=AtfParser(start=start).parser
       return self.parser.parse(content,lexer=self.lexer)
 
   def test_code(self):
-    artifact=self.try_parse("&X001001 = JCS 48, 089\n")
+    artifact=self.try_parse("&X001001 = JCS 48, 089\n","code")
     assert_is_instance(artifact,Artifact)
     assert_equal(artifact.description,"JCS 48, 089")
     assert_equal(artifact.code,"X001001")
