3415ede1c4ff4f502650263b9f963dacdc07acdf
James Hetherington <j.hetherington@ucl.ac.uk>
2014-06-09 16:45:01 +0100

    Wrap up searching in sample corpus for a fixture

--- pyoracc/test/fixtures/__init__.py
+++ pyoracc/test/fixtures/__init__.py
@@ -13,6 +13,9 @@ def tiny_corpus():
     return os.path.join(os.path.dirname(here),'tiny_corpus')
 
 def anzu():
+    return sample_file("anzu")
+
+def sample_file(name):
     return codecs.open(os.path.join(
-                       os.path.dirname(here),'sample_corpus',"anzu.atf"),
+                       os.path.dirname(here),'sample_corpus',name+".atf"),
                        encoding='utf-8').read()
