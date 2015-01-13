e605e7b15c449d65303a32d3b9996868295fb3ae
James Hetherington <jamespjh@gmail.com>
2015-01-13 10:53:43 +0000

    Pep8

--- pyoracc/atf/atffile.py
+++ pyoracc/atf/atffile.py
@@ -7,7 +7,7 @@ class AtfFile(object):
     def __init__(self, content):
         self.content = content
         if content[-1] != '\n':
-            content+="\n"
+            content += "\n"
         lexer = AtfLexer().lexer
         parser = AtfParser().parser
         self.text = parser.parse(content, lexer=lexer)
