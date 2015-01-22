0b85886f3537e253a3c1cc6b4e11284eced07568
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-01-22 11:48:02 +0000

    Start serializing from this object instead of text.

--- pyoracc/atf/atffile.py
+++ pyoracc/atf/atffile.py
@@ -1,8 +1,13 @@
 from atflex import AtfLexer
 from atfyacc import AtfParser
+from mako.template import Template
+
 
 
 class AtfFile(object):
+    
+    template = Template("${text}")
+
     def __init__(self, content):
         self.content = content
         if content[-1] != '\n':
@@ -10,3 +15,8 @@ class AtfFile(object):
         lexer = AtfLexer().lexer
         parser = AtfParser().parser
         self.text = parser.parse(content, lexer=lexer)
+        
+    
+        
+    def __str__(self):
+        return AtfFile.template.render(**vars(self))
