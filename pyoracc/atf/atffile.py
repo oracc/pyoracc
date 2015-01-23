d8571d94b0bb50d40a8e8c0178ef70c757a92901
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-01-23 17:23:35 +0000

    str() won't produce unicode strings, so a new dedicated serializer method had to be created.

--- pyoracc/atf/atffile.py
+++ pyoracc/atf/atffile.py
@@ -6,7 +6,7 @@ from mako.template import Template
 
 class AtfFile(object):
     
-    template = Template("${text}")
+    template = Template("${text.serialize()}")
 
     def __init__(self, content):
         self.content = content
@@ -15,8 +15,10 @@ class AtfFile(object):
         lexer = AtfLexer().lexer
         parser = AtfParser().parser
         self.text = parser.parse(content, lexer=lexer)
-        
-    
-        
+           
     def __str__(self):
-        return AtfFile.template.render(**vars(self))
+        return AtfFile.template.render_unicode(**vars(self))
+    
+    def serialize(self):
+        return AtfFile.template.render_unicode(**vars(self))
+    
