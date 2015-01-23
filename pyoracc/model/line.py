cb98e5d86c105e3e6fad8a9f19761f5b11f365f6
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-01-23 17:24:17 +0000

    str() won't produce unicode strings, so a new dedicated serializer method had to be created. Also Mako template reworked to print words and lemmas.

--- pyoracc/model/line.py
+++ pyoracc/model/line.py
@@ -1,9 +1,19 @@
 from mako.template import Template
 
 class Line(object):
-    template = Template("""${label}. ${words}
-#lem: ${lemmas}\n""")
-    
+    template = Template("""${label}. \\
+% for word in words:
+${word} \\
+% endfor 
+% if lemmas:
+\n#lem: \\
+% for lemma in lemmas:
+${lemma}; \\
+% endfor \n
+%endif
+""", output_encoding='utf-8')
+
+
     def __init__(self, label):
         self.label = label
         self.words = []
@@ -15,4 +25,7 @@ class Line(object):
         self.links = []
         
     def __str__(self):
-        return self.template.render(**vars(self))
+        return self.template.render_unicode(**vars(self))
+    
+    def serialize(self):
+        return self.template.render_unicode(**vars(self))
