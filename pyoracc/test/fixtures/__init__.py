9ff92ac85879fc2d9844f5b602f76e66855b92a4
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-01-26 17:35:31 +0000

    Make 80-char lines.

--- pyoracc/test/fixtures/__init__.py
+++ pyoracc/test/fixtures/__init__.py
@@ -34,4 +34,7 @@ def output_folder():
 
 def output_filepath(atf_filename):
     atf_without_ext = re.sub('.atf$', '', atf_filename)
-    return os.path.join(output_folder(), atf_without_ext + "_" + time.strftime("%Y%m%d%H%M%S")  + ".atf")
+    return os.path.join(output_folder(), atf_without_ext + "_" + 
+                        time.strftime("%Y%m%d%H%M%S")  + ".atf")
+#return os.path.join(output_folder(), atf_filename)
+
