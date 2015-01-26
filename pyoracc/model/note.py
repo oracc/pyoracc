67fadf97b7c450a193163daebbe3064697f03fa7
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-01-26 16:26:26 +0000

    Serialization for notes added.

--- pyoracc/model/note.py
+++ pyoracc/model/note.py
@@ -1,4 +1,18 @@
+from mako.template import Template
+
 class Note(object):
+    template = Template("""\\
+% if references:
+% for reference in references:
+@note ^${reference}^ ${content} 
+% endfor
+% else:
+#note: ${content}
+% endif""")
+
     def __init__(self, content=""):
         self.content = content
         self.references = []
+    
+    def serialize(self):
+        return self.template.render_unicode(**vars(self))
