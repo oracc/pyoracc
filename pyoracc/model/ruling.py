f647aa6d511f7c2c6e9bafea0add0588bc0100f9
Raquel Alegre <r.alegre@ucl.ac.uk>
2015-01-26 16:32:24 +0000

    Translate ruling count to string (single, double, triple)

--- pyoracc/model/ruling.py
+++ pyoracc/model/ruling.py
@@ -1,10 +1,11 @@
 from mako.template import Template
 
 class Ruling(object):
-    template = Template("""$ single ruling\n""")
+    template = Template("""\n$ ${type} ruling""")
     
     def __init__(self, count):
         self.count = count
+        self.type = self.getRulingType()
         self.query = False
         self.broken = False
         self.remarkable = False
@@ -15,3 +16,14 @@ class Ruling(object):
     
     def serialize(self):
         return self.template.render_unicode(**vars(self))
+    
+    def getRulingType(self):
+        typeArr = [ "single", "double", "triple"]
+        try:
+            return typeArr[self.count - 1]
+        except TypeError:
+            print("Error: Ruling count " + self.count + " must be an integer.")
+        except IndexError:
+            print("Error: Ruling count (" + self.count + ") is out of bounds (" + typeArr.__len__() + ").")
+            
+      
