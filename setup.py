a5cdf2ba861be750d601e4341e0a22591d2075ae
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-01-27 14:20:57 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(name='pyoracc',
       author='James Hetherington',
       author_email='j.hetherington@ucl.ac.uk',
       url='https://github.com/ucl/pyoracc',
-      download_url='https://github.com/ucl/pyoracc/archive/master.tar.gz'
+      download_url='https://github.com/ucl/pyoracc/archive/master.tar.gz',
+      packages=['pyoracc',
+                'pyoracc/atf',
+                'pyoracc/model',
+                'pyoracc/test',
+                'pyoracc/test/atf',
+                'pyoracc/test/fixtures'],
+      package_data={'pyoracc': ['test/fixtures/*/*.atf']}
       )
