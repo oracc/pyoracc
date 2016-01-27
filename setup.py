b3824ea2d90308a840082a7f10b389dadd5e8d29
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-01-27 13:33:48 +0000

    Add install script for pyoracc

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pyoracc',
+      version='0.0.1',
+      author='James Hetherington',
+      author_email='j.hetherington@ucl.ac.uk',
+      url='https://github.com/ucl/pyoracc',
+      download_url='https://github.com/ucl/pyoracc/archive/master.tar.gz'
+      )
