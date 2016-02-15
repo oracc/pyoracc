beaedfc97265fd9f8996cccc8732eed128469747
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-02-15 11:30:26 +0000

    Generate parsetab as part of the install

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+
+# Generate the parsetab file so that we can install that too
+from pyoracc import _generate_parsetab
+_generate_parsetab()
+
 setup(name='pyoracc',
       version='0.0.1',
       author='James Hetherington',
