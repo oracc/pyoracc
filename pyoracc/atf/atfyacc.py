72a773a4b875300dd97d9870231d3354444d140d
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-06-17 20:22:52 +0100

    add support for more slighly off dollar lines

--- pyoracc/atf/atfyacc.py
+++ pyoracc/atf/atfyacc.py
@@ -504,6 +504,16 @@ class AtfParser(object):
         # the same as "2 lines broken"
         p[0] = State(p[3], p[2], p[1])
 
+    def p_plural_state_description_unquantified(self, p):
+        """plural_state_description : plural_scope state
+        """
+        p[0] = State(p[2], p[1])
+
+    def p_plural_state_description_unquantified_reverse(self, p):
+        """plural_state_description : state plural_scope
+        """
+        p[0] = State(p[1], p[2])
+
     def p_plural_state_range_description(self, p):
         """plural_state_description : ID MINUS ID plural_scope state"""
         p[0] = State(p[5], p[4], p[1] + "-" + p[3])
