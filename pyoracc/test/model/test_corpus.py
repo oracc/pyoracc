897deba8cd821d44ccbe08062c88dd4a9350dd8a
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-06-20 13:37:12 +0100

    Make sure @i{ does not terminate para state

--- pyoracc/test/model/test_corpus.py
+++ pyoracc/test/model/test_corpus.py
@@ -32,5 +32,5 @@ def test_sample():
 def test_whole():
     corpus = Corpus(source=whole_corpus())
     # there is a total of 2868 files in the corpus
-    assert corpus.successes == 2555
-    assert corpus.failures == 313
+    assert corpus.successes == 2582
+    assert corpus.failures == 286
