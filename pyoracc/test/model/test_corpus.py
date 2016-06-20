23c3cc74c9ceb3fa052c51ca34d5fb7eedc79e3e
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-06-20 14:10:39 +0100

    Update corpus test

--- pyoracc/test/model/test_corpus.py
+++ pyoracc/test/model/test_corpus.py
@@ -32,5 +32,5 @@ def test_sample():
 def test_whole():
     corpus = Corpus(source=whole_corpus())
     # there is a total of 2868 files in the corpus
-    assert corpus.successes == 2582
-    assert corpus.failures == 286
+    assert corpus.successes == 2613
+    assert corpus.failures == 255
