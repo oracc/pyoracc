a6e7c26f964763288bf8df0c74e3f99544aa5b11
jayanth <jayanthjaiswal15@gmail.com>
2018-03-23 09:48:32 -0700

    Added command line utility on top of it. (#2)

--- README.md
+++ README.md
@@ -5,7 +5,91 @@ Python tools for working with ORACC
 
 Depends on PLY, Mako and Pytest
 
-## Usage
+# Installation
+
+If you don't use `pip`, you're missing out.
+Here are [installation instructions](https://pip.pypa.io/en/stable/installing/).
+
+Simply run:
+
+```bash
+    $ cd pyoracc
+    $ git pull origin master
+    $ pip install .
+```
+
+Or you can just do
+
+    $ pip install git+git://github.com/cdli-gh/pyoracc.git 
+
+Or you can also do
+
+    $ pip install git+https://github.com/cdli-gh/pyoracc.git 
+
+
+# Upgrading
+
+If you already have installed it and want to upgrade the tool:
+
+```bash
+    $ cd pyoracc
+    $ git pull origin master
+    $ pip install . --upgrade
+```
+
+Or you can just do
+
+    $ pip install git+git://github.com/cdli-gh/pyoracc.git --upgrade
+
+Or you can also do
+
+    $ pip install git+https://github.com/cdli-gh/pyoracc.git --upgrade
+
+
+# Usage
+
+To use it:
+
+    $ pyoracc --help
+
+*Only files with the .atf extension can be processed.  *
+ 
+To run it on file:
+
+    $ pyoracc -i ./pyoracc/test/data/cdli_atf_20180104.atf -f cdli
+    
+To run it on oracc file:
+
+    $ pyoracc -i ./pyoracc/test/data/cdli_atf_20180104.atf -f oracc
+
+To run it on folder:
+
+    $ pyoracc -i ./pyoracc/test/data -f cdli
+
+To see the console messages of the tool, use --verbose switch
+
+    $ pyoracc -i ./pyoracc/test/data -f cdli --verbose
+
+If you don't give arguments, it will prompt for the path and atf file type.  
+
+# Help
+
+```bash
+$ pyoracc --help
+Usage: pyoracc [OPTIONS]
+
+  My Tool does one work, and one work well.
+
+Options:
+  -i, --input_path PATH      Input the file/folder name.  [required]
+  -f, --atf_type [cdli|atf]  Input the atf file type.  [required]
+  -v, --verbose              Enables verbose mode
+  --version                  Show the version and exit.
+  --help                     Show this message and exit.
+
+```
+
+## Internal Dev Usage and Test
 
 ### To run on directory
 
--- setup.py
+++ setup.py
@@ -17,14 +17,16 @@ You should have received a copy of the GNU General Public License
 along with PyORACC. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-
-from setuptools import setup
+from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py
+
+dependencies = ['click', 'mako', 'ply']
 
 
 class MyBuildPy(build_py):
     """We subclass build_py so that we can run _generate_parsetab after
        installing the dependencies (Ply and Mako)"""
+
     def run(self):
         """Generate the parsetab file so that we can install that too before
         calling the regular installer in the super class"""
@@ -34,21 +36,43 @@ class MyBuildPy(build_py):
         # Jython plugin setuptools version 0.6...
         build_py.run(self)
 
+
 setup(name='pyoracc',
-      version='0.0.1',
+      version='0.0.2',
       author='James Hetherington',
       author_email='j.hetherington@ucl.ac.uk',
       url='https://github.com/ucl/pyoracc',
       download_url='https://github.com/ucl/pyoracc/archive/master.tar.gz',
-      packages=['pyoracc',
-                'pyoracc/atf',
-                'pyoracc/model',
-                'pyoracc/test',
-                'pyoracc/test/atf',
-                'pyoracc/test/fixtures'],
-      install_requires=['mako', 'ply'],
-      setup_requires=['mako', 'ply'],
+      packages=find_packages(exclude=['test']),
+      install_requires=dependencies,
+      setup_requires=dependencies,
       package_data={'pyoracc': ['test/fixtures/*/*.atf']},
       zip_safe=False,
-      cmdclass=dict(build_py=MyBuildPy)
+      cmdclass=dict(build_py=MyBuildPy),
+      entry_points={
+          'console_scripts': [
+              'pyoracc = pyoracc.wrapper.cli:main',
+          ],
+      },
+      classifiers=[
+          # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+          # 'Development Status :: 1 - Planning',
+          # 'Development Status :: 2 - Pre-Alpha',
+          # 'Development Status :: 3 - Alpha',
+          'Development Status :: 4 - Beta',
+          # 'Development Status :: 5 - Production/Stable',
+          # 'Development Status :: 6 - Mature',
+          # 'Development Status :: 7 - Inactive',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS',
+          'Operating System :: Unix',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ]
       )
