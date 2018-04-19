c279a1fbdbe40ecbbf3e78105d7d8cc6c0f88f57
jayanth <jayanthjaiswal15@gmail.com>
2018-04-19 04:19:08 -0700

    Version added for release

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class MyBuildPy(build_py):
 
 
 setup(name='pyoracc',
-      version='0.0.9',
+      version='0.1.0',
       author='James Hetherington',
       author_email='j.hetherington@ucl.ac.uk',
       url='https://github.com/ucl/pyoracc',
