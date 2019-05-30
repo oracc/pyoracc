48296b2194cb76bbe5a6f3d627177743c82ad043
Ralph Giles <giles@thaumas.net>
2019-05-30 11:32:56 -0700

    Always run slow tests.
    Remove the --runslow option from the pytest config and instead
    always run the slow tests. Unless a large corpus is passed
    through the `oracc_corpus_path` environment variable, this
    only adds the included sample corpus. That adds about 60%
    to the pytest runtime, but is still under 10 seconds.
    
    The motivation for this is that the way the option was accessed
    is deprecated in pytest 4.1.
    
      https://docs.pytest.org/en/latest/deprecations.html#pytest-config-global
    
    Having less configuration is less code to worry about, and less
    variance between different test invocations, so removing the
    option to skip the sample corpus is the most straightforward fix.

--- pyoracc/test/model/test_corpus.py
+++ pyoracc/test/model/test_corpus.py
@@ -25,19 +25,12 @@ from ...model.corpus import Corpus
 from ..fixtures import tiny_corpus, sample_corpus, whole_corpus
 
 
-slow = pytest.mark.skipif(
-    not pytest.config.getoption("--runslow"),
-    reason="need --runslow option to run"
-)
-
-
 def test_tiny():
     corpus = Corpus(source=tiny_corpus(), atftype='oracc')
     assert corpus.successes == 1
     assert corpus.failures == 1
 
 
-@slow
 def test_sample():
     corpus = Corpus(source=sample_corpus(), atftype='oracc')
     assert corpus.successes == 37
@@ -48,7 +41,6 @@ def test_sample():
                     reason="Need to set oracc_corpus_path to point "
                            "to the whole corpus, which is not bundled with "
                            "pyoracc")
-@slow
 def test_whole():
     corpus = Corpus(source=whole_corpus(), atftype='oracc')
     # There are a total of 8229 files in the corpus.
